d1c89a3b2bdc735623597de3d7c0470ec4593caa
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2022-11-09 11:11:53 +0300

    Update gitopia.sh

--- gitopia/gitopia.sh
+++ gitopia/gitopia.sh
@@ -14,18 +14,23 @@ echo -e "\e[0m"
 sleep 2
 
 # set vars
-if [ ! $NODENAME ]; then
-	read -p "Enter node name: " NODENAME
-	echo 'export NODENAME='$NODENAME >> $HOME/.bash_profile
+if [ ! $GIT_NODENAME ]; then
+	read -p "Enter node name: " GIT_NODENAME
+	echo 'export GIT_NODENAME='$GIT_NODENAME >> $HOME/.bash_profile
+fi
+if [ ! $GIT_PORT ]; then
+	read -p "Enter node port: " GIT_PORT
+	echo 'export GIT_PORT='$GIT_PORT >> $HOME/.bash_profile
 fi
 echo "export WALLET=wallet" >> $HOME/.bash_profile
-echo "export CHAIN_ID=gitopia-janus-testnet" >> $HOME/.bash_profile
+echo "export GITCHAIN_ID=gitopia-janus-testnet" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 
 echo '================================================='
-echo 'Your node name: ' $NODENAME
+echo 'Your node name: ' $GIT_NODENAME
+echo 'Your port num: ' $GIT_PORT
 echo 'Your wallet name: ' $WALLET
-echo 'Your chain name: ' $CHAIN_ID
+echo 'Your chain name: ' $GITCHAIN_ID
 echo '================================================='
 sleep 2
 
@@ -51,33 +56,34 @@ curl https://get.gitopia.com | bash
 
 # download binary
 cd $HOME
-git clone gitopia://gitopia1dlpc7ps63kj5v0kn5v8eq9sn2n8v8r5z9jmwff/gitopia
-cd gitopia
-git checkout main
-make install
+git clone -b v1.2.0 gitopia://gitopia/gitopia
+cd gitopia && make install
 
 # config
-gitopiad config chain-id $CHAIN_ID
-gitopiad config keyring-backend file
+gitopiad config chain-id $GITCHAIN_ID
+gitopiad config keyring-backend test
+gitopiad config node tcp://localhost:${GIT_PORT}657
 
 # init
-gitopiad init $NODENAME --chain-id $CHAIN_ID
+gitopiad init $GIT_NODENAME --chain-id $GITCHAIN_ID
 
 # download addrbook and genesis
 cd $HOME
-git clone gitopia://gitopia1dlpc7ps63kj5v0kn5v8eq9sn2n8v8r5z9jmwff/testnets
-cp $HOME/testnets/gitopia-janus-testnet/genesis.json $HOME/.gitopia/config/
+wget https://server.gitopia.com/raw/gitopia/testnets/master/gitopia-janus-testnet-2/genesis.json.gz
+gunzip genesis.json.gz
+mv genesis.json $HOME/.gitopia/config/genesis.json
 
 # set minimum gas price
 sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.001utlore\"/" $HOME/.gitopia/config/app.toml
 
 # set peers and seeds
-SEEDS=""
+SEEDS="399d4e19186577b04c23296c4f7ecc53e61080cb@seed.gitopia.com:26656"
 PEERS=""
 sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.gitopia/config/config.toml
 
-# enable prometheus
-sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.gitopia/config/config.toml
+sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:${GIT_PORT}658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:${GIT_PORT}657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:${GIT_PORT}060\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:${GIT_PORT}656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":${GIT_PORT}660\"%" $HOME/.gitopia/config/config.toml
+sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:${GIT_PORT}317\"%; s%^address = \":8080\"%address = \":${GIT_PORT}080\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:${GIT_PORT}090\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:${GIT_PORT}091\"%; s%^address = \"0.0.0.0:8545\"%address = \"0.0.0.0:${GIT_PORT}545\"%; s%^ws-address = \"0.0.0.0:8546\"%ws-address = \"0.0.0.0:${GIT_PORT}546\"%" $HOME/.gitopia/config/app.toml
+
 
 # reset
 gitopiad unsafe-reset-all
