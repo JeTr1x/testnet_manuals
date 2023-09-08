afedb37587e765f22147d76ef52911c9e0bbf4a0
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2023-09-08 17:46:20 +0300

    Update celestia.sh

--- celestia/celestia.sh
+++ celestia/celestia.sh
@@ -15,141 +15,84 @@ echo -e "\e[0m"
 sleep 2
 
 # set vars
-if [ ! $CLST_NODENAME ]; then
-	read -p "Enter node name: " CLST_NODENAME
-	echo 'export CLST_NODENAME='$CLST_NODENAME >> $HOME/.bash_profile
-fi
-if [ ! $CLST_PORT ]; then
-	read -p "Enter port number: " CLST_PORT
-	echo 'export CLST_PORT='$CLST_PORT >> $HOME/.bash_profile
-fi
-if [ ! $WALLET ]; then
-	echo "export WALLET=wallet" >> $HOME/.bash_profile
-fi
-echo "export CELESTIA_CHAIN_ID=mocha" >> $HOME/.bash_profile
-echo "export CLST_PORT=${CLST_PORT}" >> $HOME/.bash_profile
-source $HOME/.bash_profile
+read -p "Enter node name: " MONIKERCEL
 
-echo '================================================='
-echo -e "Your node name: \e[1m\e[32m$CLST_NODENAME\e[0m"
-echo -e "Your wallet name: \e[1m\e[32m$WALLET\e[0m"
-echo -e "Your chain name: \e[1m\e[32m$CELESTIA_CHAIN_ID\e[0m"
-echo -e "Your port: \e[1m\e[32m$CLST_PORT\e[0m"
-echo '================================================='
-sleep 2
-
-echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
-# update
-sudo apt update && sudo apt upgrade -y
-
-echo -e "\e[1m\e[32m2. Installing dependencies... \e[0m" && sleep 1
-# packages
-sudo apt install curl build-essential git wget jq make gcc tmux chrony -y
-
-# install go
-ver="1.18.2"
-cd $HOME
-wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
 sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
-rm "go$ver.linux-amd64.tar.gz"
-echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
-source ~/.bash_profile
-go version
+curl -Ls https://go.dev/dl/go1.21.1.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
+eval $(echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh)
+eval $(echo 'export PATH=$PATH:$HOME/go/bin' | tee -a $HOME/.profile)
 
-echo -e "\e[1m\e[32m3. Downloading and building binaries... \e[0m" && sleep 1
-# download binary
 cd $HOME
-rm -rf celestia-app
+systemctl stop celestia-appd
+rm -rf celestia-app .celestia-app
 git clone https://github.com/celestiaorg/celestia-app.git
-cd celestia-app/
-APP_VERSION=v0.11.0
-git checkout tags/$APP_VERSION -b $APP_VERSION
-make install
-
-# download network tools
-cd $HOME
-rm -rf networks
-git clone https://github.com/celestiaorg/networks.git
-
-# config
-celestia-appd config chain-id $CELESTIA_CHAIN_ID
-celestia-appd config keyring-backend test
-celestia-appd config node tcp://localhost:${CLST_PORT}657
-
-# init
-celestia-appd init $CLST_NODENAME --chain-id $CELESTIA_CHAIN_ID
-
-# download genesis and addrbook
-cp $HOME/networks/mocha/genesis.json $HOME/.celestia-app/config
-
-
-MY_PEER=$(celestia-appd tendermint show-node-id)@$(curl -s ifconfig.me)$(grep -A 9 "\[p2p\]" ~/.celestia-app/config/config.toml | egrep -o ":[0-9]+")
-PEERS=$(curl -sL https://raw.githubusercontent.com/celestiaorg/networks/master/mocha/peers.txt | tr -d '\n' | head -c -1 | sed s/"$MY_PEER"// | sed "s/,,/,/g")
-SEEDS="8084e73b70dbe7fba3602be586de45a516012e6f@144.76.112.238:26656"
-echo $MY_PEER $PEERS $SEEDS
-sed -i.bak -e "s/^persistent-peers *=.*/persistent-peers = \"$PEERS\"/" $HOME/.celestia-app/config/config.toml
-sed -i.bak -e "s/^seeds *=.*/seeds = \"$SEEDS\"/" $HOME/.celestia-app/config/config.toml
-
-
-# set custom ports
-sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:${CLST_PORT}658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:${CLST_PORT}657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:${CLST_PORT}060\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:${CLST_PORT}656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":${CLST_PORT}660\"%" $HOME/.celestia-app/config/config.toml
-sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:${CLST_PORT}317\"%; s%^address = \":8080\"%address = \":${CLST_PORT}080\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:${CLST_PORT}090\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:${CLST_PORT}091\"%" $HOME/.celestia-app/config/app.toml
-
-# config pruning
-pruning="custom"
-pruning_keep_recent="100"
-pruning_keep_every="0"
-pruning_interval="10"
-sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/.celestia-app/config/app.toml
-sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/.celestia-app/config/app.toml
-sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/.celestia-app/config/app.toml
-sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/.celestia-app/config/app.toml
-
-# set minimum gas price
-sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0utia\"/" $HOME/.celestia-app/config/app.toml
-
-# enable prometheus
-sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.celestia-app/config/config.toml
-
-# reset
-celestia-appd tendermint unsafe-reset-all --home $HOME/.celestia-app
-
-cd $HOME
-rm -rf ~/.celestia-app/data
-mkdir -p ~/.celestia-app/data
-SNAP_NAME=$(curl -s https://snaps.qubelabs.io/celestia/ | \
-    egrep -o ">mocha.*tar" | tr -d ">")
-wget -O - https://snaps.qubelabs.io/celestia/${SNAP_NAME} | tar xf - \
-    -C ~/.celestia-app/data/
+cd celestia-app
+git checkout v1.0.0-rc14
+make build
+mkdir -p $HOME/.celestia-app/cosmovisor/genesis/bin
+mv build/celestia-appd $HOME/.celestia-app/cosmovisor/genesis/bin/
+rm -rf build
+sudo ln -s $HOME/.celestia-app/cosmovisor/genesis $HOME/.celestia-app/cosmovisor/current -f
+sudo ln -s $HOME/.celestia-app/cosmovisor/current/bin/celestia-appd /usr/local/bin/celestia-appd -f
+go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.4.0
 
 
 
-
-echo -e "\e[1m\e[32m4. Starting service... \e[0m" && sleep 1
-# create service
-sudo tee /etc/systemd/system/celestiad.service > /dev/null <<EOF
+sudo tee /etc/systemd/system/celestia-appd.service > /dev/null << EOF
 [Unit]
-Description=celestia
+Description=celestia node service
 After=network-online.target
 
 [Service]
 User=$USER
-ExecStart=$(which celestia-appd) start --home $HOME/.celestia-app
+ExecStart=$(which cosmovisor) run start
 Restart=on-failure
-RestartSec=3
+RestartSec=10
 LimitNOFILE=65535
+Environment="DAEMON_HOME=$HOME/.celestia-app"
+Environment="DAEMON_NAME=celestia-appd"
+Environment="UNSAFE_SKIP_BACKUP=true"
+Environment="PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:$HOME/.celestia-app/cosmovisor/current/bin"
 
 [Install]
 WantedBy=multi-user.target
 EOF
-
-# start service
 sudo systemctl daemon-reload
-sudo systemctl enable celestiad
-sudo systemctl restart celestiad
+sudo systemctl enable celestia-appd
 
-echo '=============== SETUP FINISHED ==================='
-echo -e 'To check logs: \e[1m\e[32mjournalctl -u celestiad -f -o cat\e[0m'
-echo -e "To check sync status: \e[1m\e[32mcurl -s localhost:${CLST_PORT}657/status | jq .result.sync_info\e[0m"
-journalctl -u celestiad -f -o cat
+
+celestia-appd config chain-id mocha-4
+celestia-appd config keyring-backend test
+celestia-appd config node tcp://localhost:12057
+
+# Initialize the node
+celestia-appd init $MONIKERCEL --chain-id mocha-4
+
+
+curl -Ls https://snapshots.kjnodes.com/celestia-testnet/genesis.json > $HOME/.celestia-app/config/genesis.json
+curl -Ls https://snapshots.kjnodes.com/celestia-testnet/addrbook.json > $HOME/.celestia-app/config/addrbook.json
+
+
+
+sed -i -e "s|^seeds *=.*|seeds = \"3f472746f46493309650e5a033076689996c8881@celestia-testnet.rpc.kjnodes.com:12059\"|" $HOME/.celestia-app/config/config.toml
+
+# Set commit timeout
+sed -i -e "s|^target_height_duration *=.*|timeout_commit = \"11s\"|" $HOME/.celestia-app/config/config.toml
+
+# Set minimum gas price
+sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"0.005utia\"|" $HOME/.celestia-app/config/app.toml
+
+# Set pruning
+sed -i \
+  -e 's|^pruning *=.*|pruning = "nothing"|' \
+  $HOME/.celestia-app/config/app.toml
+
+# Set custom ports
+sed -i -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:12058\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:12057\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:12060\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:12056\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":12066\"%" $HOME/.celestia-app/config/config.toml
+sed -i -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:12017\"%; s%^address = \":8080\"%address = \":12080\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:12090\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:12091\"%; s%:8545%:12045%; s%:8546%:12046%; s%:6065%:12065%" $HOME/.celestia-app/config/app.toml
+
+
+curl -L https://snapshots.kjnodes.com/celestia-testnet/snapshot_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.celestia-app
+[[ -f $HOME/.celestia-app/data/upgrade-info.json ]] && cp $HOME/.celestia-app/data/upgrade-info.json $HOME/.celestia-app/cosmovisor/genesis/upgrade-info.json
+
+sudo systemctl start celestia-appd && sudo journalctl -u celestia-appd -f --no-hostname -o cat
