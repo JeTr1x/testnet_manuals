07070191668d4f38b533b5840e5895d209160d62
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2024-05-06 19:58:53 +0300

    Create warden.sh

---
+++ warden/warden.sh
@@ -0,0 +1,168 @@
+#!/bin/bash
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
+if exists curl; then
+echo ''
+else
+  sudo apt update && sudo apt install curl -y < "/dev/null"
+fi
+bash_profile=$HOME/.bash_profile
+if [ -f "$bash_profile" ]; then
+    . $HOME/.bash_profile
+fi
+sleep 1 && curl -s https://api.nodes.guru/logo.sh | bash && sleep 1
+
+NODE="wardend"
+export DAEMON_HOME=$HOME/.warden
+export DAEMON_NAME=wardend
+if [ -d "$DAEMON_HOME" ]; then
+    new_folder_name="${DAEMON_HOME}_$(date +"%Y%m%d_%H%M%S")"
+    mv "$DAEMON_HOME" "$new_folder_name"
+fi
+#CHAIN_ID=zgtendermint_9000-1
+#echo 'export CHAIN_ID='\"${CHAIN_ID}\" >> $HOME/.bash_profile
+
+if [ ! $WARDENVALIDATOR ]; then
+    read -p "Enter validator name: " WARDENVALIDATOR
+    echo 'export WARDENVALIDATOR='\"${WARDENVALIDATOR}\" >> $HOME/.bash_profile
+fi
+echo 'source $HOME/.bashrc' >> $HOME/.bash_profile
+source $HOME/.bash_profile
+sleep 1
+cd $HOME
+sudo apt update
+sudo apt install make unzip clang pkg-config lz4 libssl-dev build-essential git jq ncdu bsdmainutils htop -y < "/dev/null"
+
+echo -e '\n\e[42mInstall Go\e[0m\n' && sleep 1
+cd $HOME
+VERSION=1.21.9
+wget -O go.tar.gz https://go.dev/dl/go$VERSION.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go.tar.gz && rm go.tar.gz
+echo 'export GOROOT=/usr/local/go' >> $HOME/.bash_profile
+echo 'export GOPATH=$HOME/go' >> $HOME/.bash_profile
+echo 'export GO111MODULE=on' >> $HOME/.bash_profile
+echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile && . $HOME/.bash_profile
+go version
+
+echo -e '\n\e[42mInstall software\e[0m\n' && sleep 1
+
+sleep 1
+source .profile
+cd $HOME
+rm -rf wardenprotocol
+git clone --depth 1 --branch v0.3.0 https://github.com/warden-protocol/wardenprotocol/
+cd wardenprotocol
+make build
+sudo mv ./build/wardend /usr/local/bin/
+
+#SEEDS="8c01665f88896bca44e8902a30e4278bed08033f@54.241.167.190:26656,b288e8b37f4b0dbd9a03e8ce926cd9c801aacf27@54.176.175.48:26656,8e20e8e88d504e67c7a3a58c2ea31d965aa2a890@54.193.250.204:26656,e50ac888b35175bfd4f999697bdeb5b7b52bfc06@54.215.187.94:26656"
+$DAEMON_NAME init $WARDENVALIDATOR --chain-id buenavista-1
+sleep 1
+$DAEMON_NAME config set client chain-id buenavista-1
+$DAEMON_NAME config set client keyring-backend test
+wget -O $HOME/.warden/config/genesis.json https://raw.githubusercontent.com/warden-protocol/networks/main/testnets/buenavista/genesis.json
+#sed -i.bak -e "s/^seeds *=.*/seeds = \"${SEEDS}\"/" $DAEMON_HOME/config/config.toml
+sed -i 's/^minimum-gas-prices *=.*/minimum-gas-prices = "0.0025uward"/' $DAEMON_HOME/config/app.toml
+sed -i 's/^persistent_peers *=.*/persistent_peers = "ddb4d92ab6eba8363bab2f3a0d7fa7a970ae437f@sentry-1.buenavista.wardenprotocol.org:26656,c717995fd56dcf0056ed835e489788af4ffd8fe8@sentry-2.buenavista.wardenprotocol.org:26656,e1c61de5d437f35a715ac94b88ec62c482edc166@sentry-3.buenavista.wardenprotocol.org:26656"/' $DAEMON_HOME/config/config.toml
+
+echo "[Unit]
+Description=$NODE Node
+After=network.target
+
+[Service]
+User=$USER
+Type=simple
+ExecStart=/usr/local/bin/$DAEMON_NAME start
+Restart=on-failure
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target" > $HOME/$NODE.service
+sudo mv $HOME/$NODE.service /etc/systemd/system
+sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
+Storage=persistent
+EOF
+
+echo -e '\n\e[42mDownloading a snapshot\e[0m\n' && sleep 1
+curl https://snapshots.nodes.guru/warden/latest_snapshot.tar.lz4 | lz4 -dc - | tar -xf - -C $DAEMON_HOME
+wget -O $DAEMON_HOME/config/addrbook.json https://snapshots.nodes.guru/warden/addrbook.json
+
+echo -e '\n\e[42mChecking a ports\e[0m\n' && sleep 1
+#CHECK PORTS
+PORT=336
+if ss -tulpen | awk '{print $5}' | grep -q ":26656$" ; then
+    echo -e "\e[31mPort 26656 already in use.\e[39m"
+    sleep 2
+    sed -i -e "s|:26656\"|:${PORT}56\"|g" $DAEMON_HOME/config/config.toml
+    echo -e "\n\e[42mPort 26656 changed to ${PORT}56.\e[0m\n"
+    sleep 2
+fi
+if ss -tulpen | awk '{print $5}' | grep -q ":26657$" ; then
+    echo -e "\e[31mPort 26657 already in use\e[39m"
+    sleep 2
+    sed -i -e "s|:26657\"|:${PORT}57\"|" $DAEMON_HOME/config/config.toml
+    echo -e "\n\e[42mPort 26657 changed to ${PORT}57.\e[0m\n"
+    sleep 2
+    $DAEMON_NAME config set client node tcp://localhost:${PORT}57
+fi
+if ss -tulpen | awk '{print $5}' | grep -q ":26658$" ; then
+    echo -e "\e[31mPort 26658 already in use.\e[39m"
+    sleep 2
+    sed -i -e "s|:26658\"|:${PORT}58\"|" $DAEMON_HOME/config/config.toml
+    echo -e "\n\e[42mPort 26658 changed to ${PORT}58.\e[0m\n"
+    sleep 2
+fi
+if ss -tulpen | awk '{print $5}' | grep -q ":6060$" ; then
+    echo -e "\e[31mPort 6060 already in use.\e[39m"
+    sleep 2
+    sed -i -e "s|:6060\"|:${PORT}60\"|" $DAEMON_HOME/config/config.toml
+    echo -e "\n\e[42mPort 6060 changed to ${PORT}60.\e[0m\n"
+    sleep 2
+fi
+if ss -tulpen | awk '{print $5}' | grep -q ":1317$" ; then
+    echo -e "\e[31mPort 1317 already in use.\e[39m"
+    sleep 2
+    sed -i -e "s|:1317\"|:${PORT}17\"|" $DAEMON_HOME/config/app.toml
+    echo -e "\n\e[42mPort 1317 changed to ${PORT}17.\e[0m\n"
+    sleep 2
+fi
+if ss -tulpen | awk '{print $5}' | grep -q ":9090$" ; then
+    echo -e "\e[31mPort 9090 already in use.\e[39m"
+    sleep 2
+    sed -i -e "s|:9090\"|:${PORT}90\"|" $DAEMON_HOME/config/app.toml
+    echo -e "\n\e[42mPort 9090 changed to ${PORT}90.\e[0m\n"
+    sleep 2
+fi
+
+#echo -e '\n\e[42mRunning a service\e[0m\n' && sleep 1
+sudo systemctl restart systemd-journald
+sudo systemctl daemon-reload
+sudo systemctl enable $NODE
+sudo systemctl restart $NODE
+
+tee $DAEMON_HOME/validator.json > /dev/null << EOF
+{
+    "pubkey": $(wardend comet show-validator),
+    "amount": "1000000uward",
+    "moniker": "${WARDENVALIDATOR}",
+    "identity": "",
+    "website": "",
+    "security": "",
+    "details": "",
+    "commission-rate": "0.1",
+    "commission-max-rate": "0.2",
+    "commission-max-change-rate": "0.01",
+    "min-self-delegation": "1"
+}
+EOF
+
+echo -e '\n\e[42mCheck node status\e[0m\n' && sleep 1
+if [[ `service $NODE status | grep active` =~ "running" ]]; then
+  echo -e "Your $NODE node \e[32minstalled and works\e[39m!"
+  echo -e "You can check node status by the command \e[7mservice 0g status\e[0m"
+  echo -e "Press \e[7mQ\e[0m for exit from status menu"
+else
+  echo -e "Your $NODE node \e[31mwas not installed correctly\e[39m, please reinstall."
+fi
