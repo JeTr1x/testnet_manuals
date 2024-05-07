4bd4a48b00cdbfe0e3b217ffc0b4dc209f312929
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2024-05-07 14:35:32 +0300

    Update warden.sh

--- warden/warden.sh
+++ warden/warden.sh
@@ -13,12 +13,12 @@ if [ -f "$bash_profile" ]; then
     . $HOME/.bash_profile
 fi
 
-NODE="wardend"
-export DAEMON_HOME=$HOME/.warden
-export DAEMON_NAME=wardend
-if [ -d "$DAEMON_HOME" ]; then
-    new_folder_name="${DAEMON_HOME}_$(date +"%Y%m%d_%H%M%S")"
-    mv "$DAEMON_HOME" "$new_folder_name"
+WARDEN_NODE="wardend"
+export WARDEN_DAEMON_HOME=$HOME/.warden
+export WARDEN_DAEMON_NAME=wardend
+if [ -d "$WARDEN_DAEMON_HOME" ]; then
+    new_folder_name="${WARDEN_DAEMON_HOME}_$(date +"%Y%m%d_%H%M%S")"
+    mv "$WARDEN_DAEMON_HOME" "$new_folder_name"
 fi
 #CHAIN_ID=zgtendermint_9000-1
 #echo 'export CHAIN_ID='\"${CHAIN_ID}\" >> $HOME/.bash_profile
@@ -36,8 +36,8 @@ sudo apt install make unzip clang pkg-config lz4 libssl-dev build-essential git 
 
 echo -e '\n\e[42mInstall Go\e[0m\n' && sleep 1
 cd $HOME
-VERSION=1.21.9
-wget -O go.tar.gz https://go.dev/dl/go$VERSION.linux-amd64.tar.gz
+WARDEN_VERSION=1.21.9
+wget -O go.tar.gz https://go.dev/dl/go$WARDEN_VERSION.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go.tar.gz && rm go.tar.gz
 echo 'export GOROOT=/usr/local/go' >> $HOME/.bash_profile
 echo 'export GOPATH=$HOME/go' >> $HOME/.bash_profile
@@ -57,36 +57,36 @@ make build
 sudo mv ./build/wardend /usr/local/bin/
 
 #SEEDS="8c01665f88896bca44e8902a30e4278bed08033f@54.241.167.190:26656,b288e8b37f4b0dbd9a03e8ce926cd9c801aacf27@54.176.175.48:26656,8e20e8e88d504e67c7a3a58c2ea31d965aa2a890@54.193.250.204:26656,e50ac888b35175bfd4f999697bdeb5b7b52bfc06@54.215.187.94:26656"
-$DAEMON_NAME init $WARDENVALIDATOR --chain-id buenavista-1
+$WARDEN_DAEMON_NAME init $WARDENVALIDATOR --chain-id buenavista-1
 sleep 1
-$DAEMON_NAME config set client chain-id buenavista-1
-$DAEMON_NAME config set client keyring-backend test
+$WARDEN_DAEMON_NAME config set client chain-id buenavista-1
+$WARDEN_DAEMON_NAME config set client keyring-backend test
 wget -O $HOME/.warden/config/genesis.json https://raw.githubusercontent.com/warden-protocol/networks/main/testnets/buenavista/genesis.json
-#sed -i.bak -e "s/^seeds *=.*/seeds = \"${SEEDS}\"/" $DAEMON_HOME/config/config.toml
-sed -i 's/^minimum-gas-prices *=.*/minimum-gas-prices = "0.0025uward"/' $DAEMON_HOME/config/app.toml
-sed -i 's/^persistent_peers *=.*/persistent_peers = "ddb4d92ab6eba8363bab2f3a0d7fa7a970ae437f@sentry-1.buenavista.wardenprotocol.org:26656,c717995fd56dcf0056ed835e489788af4ffd8fe8@sentry-2.buenavista.wardenprotocol.org:26656,e1c61de5d437f35a715ac94b88ec62c482edc166@sentry-3.buenavista.wardenprotocol.org:26656"/' $DAEMON_HOME/config/config.toml
+#sed -i.bak -e "s/^seeds *=.*/seeds = \"${SEEDS}\"/" $WARDEN_DAEMON_HOME/config/config.toml
+sed -i 's/^minimum-gas-prices *=.*/minimum-gas-prices = "0.0025uward"/' $WARDEN_DAEMON_HOME/config/app.toml
+sed -i 's/^persistent_peers *=.*/persistent_peers = "ddb4d92ab6eba8363bab2f3a0d7fa7a970ae437f@sentry-1.buenavista.wardenprotocol.org:26656,c717995fd56dcf0056ed835e489788af4ffd8fe8@sentry-2.buenavista.wardenprotocol.org:26656,e1c61de5d437f35a715ac94b88ec62c482edc166@sentry-3.buenavista.wardenprotocol.org:26656"/' $WARDEN_DAEMON_HOME/config/config.toml
 
 echo "[Unit]
-Description=$NODE Node
+Description=$WARDEN_NODE Node
 After=network.target
 
 [Service]
 User=$USER
 Type=simple
-ExecStart=/usr/local/bin/$DAEMON_NAME start
+ExecStart=/usr/local/bin/$WARDEN_DAEMON_NAME start
 Restart=on-failure
 LimitNOFILE=65535
 
 [Install]
-WantedBy=multi-user.target" > $HOME/$NODE.service
-sudo mv $HOME/$NODE.service /etc/systemd/system
+WantedBy=multi-user.target" > $HOME/$WARDEN_NODE.service
+sudo mv $HOME/$WARDEN_NODE.service /etc/systemd/system
 sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
 Storage=persistent
 EOF
 
 echo -e '\n\e[42mDownloading a snapshot\e[0m\n' && sleep 1
-curl https://snapshots.nodes.guru/warden/latest_snapshot.tar.lz4 | lz4 -dc - | tar -xf - -C $DAEMON_HOME
-wget -O $DAEMON_HOME/config/addrbook.json https://snapshots.nodes.guru/warden/addrbook.json
+curl https://snapshots.nodes.guru/warden/latest_snapshot.tar.lz4 | lz4 -dc - | tar -xf - -C $WARDEN_DAEMON_HOME
+wget -O $WARDEN_DAEMON_HOME/config/addrbook.json https://snapshots.nodes.guru/warden/addrbook.json
 
 echo -e '\n\e[42mChecking a ports\e[0m\n' && sleep 1
 #CHECK PORTS
@@ -94,43 +94,43 @@ PORT=336
 if ss -tulpen | awk '{print $5}' | grep -q ":26656$" ; then
     echo -e "\e[31mPort 26656 already in use.\e[39m"
     sleep 2
-    sed -i -e "s|:26656\"|:${PORT}56\"|g" $DAEMON_HOME/config/config.toml
+    sed -i -e "s|:26656\"|:${PORT}56\"|g" $WARDEN_DAEMON_HOME/config/config.toml
     echo -e "\n\e[42mPort 26656 changed to ${PORT}56.\e[0m\n"
     sleep 2
 fi
 if ss -tulpen | awk '{print $5}' | grep -q ":26657$" ; then
     echo -e "\e[31mPort 26657 already in use\e[39m"
     sleep 2
-    sed -i -e "s|:26657\"|:${PORT}57\"|" $DAEMON_HOME/config/config.toml
+    sed -i -e "s|:26657\"|:${PORT}57\"|" $WARDEN_DAEMON_HOME/config/config.toml
     echo -e "\n\e[42mPort 26657 changed to ${PORT}57.\e[0m\n"
     sleep 2
-    $DAEMON_NAME config set client node tcp://localhost:${PORT}57
+    $WARDEN_DAEMON_NAME config set client node tcp://localhost:${PORT}57
 fi
 if ss -tulpen | awk '{print $5}' | grep -q ":26658$" ; then
     echo -e "\e[31mPort 26658 already in use.\e[39m"
     sleep 2
-    sed -i -e "s|:26658\"|:${PORT}58\"|" $DAEMON_HOME/config/config.toml
+    sed -i -e "s|:26658\"|:${PORT}58\"|" $WARDEN_DAEMON_HOME/config/config.toml
     echo -e "\n\e[42mPort 26658 changed to ${PORT}58.\e[0m\n"
     sleep 2
 fi
 if ss -tulpen | awk '{print $5}' | grep -q ":6060$" ; then
     echo -e "\e[31mPort 6060 already in use.\e[39m"
     sleep 2
-    sed -i -e "s|:6060\"|:${PORT}60\"|" $DAEMON_HOME/config/config.toml
+    sed -i -e "s|:6060\"|:${PORT}60\"|" $WARDEN_DAEMON_HOME/config/config.toml
     echo -e "\n\e[42mPort 6060 changed to ${PORT}60.\e[0m\n"
     sleep 2
 fi
 if ss -tulpen | awk '{print $5}' | grep -q ":1317$" ; then
     echo -e "\e[31mPort 1317 already in use.\e[39m"
     sleep 2
-    sed -i -e "s|:1317\"|:${PORT}17\"|" $DAEMON_HOME/config/app.toml
+    sed -i -e "s|:1317\"|:${PORT}17\"|" $WARDEN_DAEMON_HOME/config/app.toml
     echo -e "\n\e[42mPort 1317 changed to ${PORT}17.\e[0m\n"
     sleep 2
 fi
 if ss -tulpen | awk '{print $5}' | grep -q ":9090$" ; then
     echo -e "\e[31mPort 9090 already in use.\e[39m"
     sleep 2
-    sed -i -e "s|:9090\"|:${PORT}90\"|" $DAEMON_HOME/config/app.toml
+    sed -i -e "s|:9090\"|:${PORT}90\"|" $WARDEN_DAEMON_HOME/config/app.toml
     echo -e "\n\e[42mPort 9090 changed to ${PORT}90.\e[0m\n"
     sleep 2
 fi
@@ -138,10 +138,10 @@ fi
 #echo -e '\n\e[42mRunning a service\e[0m\n' && sleep 1
 sudo systemctl restart systemd-journald
 sudo systemctl daemon-reload
-sudo systemctl enable $NODE
-sudo systemctl restart $NODE
+sudo systemctl enable $WARDEN_NODE
+sudo systemctl restart $WARDEN_NODE
 
-tee $DAEMON_HOME/validator.json > /dev/null << EOF
+tee $WARDEN_DAEMON_HOME/validator.json > /dev/null << EOF
 {
     "pubkey": $(wardend comet show-validator),
     "amount": "1000000uward",
@@ -158,10 +158,10 @@ tee $DAEMON_HOME/validator.json > /dev/null << EOF
 EOF
 
 echo -e '\n\e[42mCheck node status\e[0m\n' && sleep 1
-if [[ `service $NODE status | grep active` =~ "running" ]]; then
-  echo -e "Your $NODE node \e[32minstalled and works\e[39m!"
+if [[ `service $WARDEN_NODE status | grep active` =~ "running" ]]; then
+  echo -e "Your $WARDEN_NODE node \e[32minstalled and works\e[39m!"
   echo -e "You can check node status by the command \e[7mservice wardend status\e[0m"
   echo -e "Press \e[7mQ\e[0m for exit from status menu"
 else
-  echo -e "Your $NODE node \e[31mwas not installed correctly\e[39m, please reinstall."
+  echo -e "Your $WARDEN_NODE node \e[31mwas not installed correctly\e[39m, please reinstall."
 fi
