59ec780884e130936d157c81bde7b704d7ee2b90
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2025-01-09 22:16:45 +0300

    Update babylon_node_inst.sh

--- babylon_node_inst.sh
+++ babylon_node_inst.sh
@@ -41,6 +41,8 @@ sed -i '/\[rpc\]/,/\[/{s/^laddr = "tcp:\/\/127\.0\.0\.1:/laddr = "tcp:\/\/0.0.0.
 curl -Ls https://raw.githubusercontent.com/babylonlabs-io/networks/030a0c7b29b1156840383e09816b4187560c41e4/bbn-test-5/network-artifacts/genesis.json > $HOME/.babylond/config/genesis.json 
 curl -Ls https://ss-t.babylon.nodestake.org/addrbook.json > $HOME/.babylond/config/addrbook.json 
 
+babylond config set client chain-id bbn-test-5
+babylond config set client node tcp://localhost:${BABYLON_PORT}657
 
 cp $HOME/.babylond/data/priv_validator_state.json $HOME/.babylond/priv_validator_state.json.backup
 rm -rf $HOME/.babylond/data
