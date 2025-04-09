f9921c609f69ae510daa762cd3fc06a4c77189c0
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2025-04-09 17:45:54 +0300

    Update swaprpc.sh

--- swaprpc.sh
+++ swaprpc.sh
@@ -5,8 +5,9 @@
 read -rp "Enter L1RPC: " L1RPC
 read -rp "Enter CHAINRPC: " CHAINRPC
 
-cat /root/mee-node-deployment/chains-testnet/* | grep rpc
 echo ""
+echo ""
+cat /root/mee-node-deployment/chains-testnet/* | grep rpc
 echo ""
 echo ""
 cd mee-node-deployment
@@ -15,7 +16,8 @@ sed -i 's|"rpc": "https://optimism-sepolia.blockpi.network/v1/rpc/6807fa355fdc93
 sed -i 's|"rpc": "https://base-sepolia.blockpi.network/v1/rpc/19b755f222187861ca7e009cfd2a3deec41a676e"|"rpc": "'$CHAINRPC'"|' /root/mee-node-deployment/chains-testnet/84532.json
 sed -i 's|"rpc": "https://arbitrum-sepolia.blockpi.network/v1/rpc/39dfe10c6563716d3d3b141908f8789b72bfa5e6"|"rpc": "'$CHAINRPC'"|' /root/mee-node-deployment/chains-testnet/421614.json
 sed -i 's|"rpc": "https://ethereum.blockpi.network/v1/rpc/73bb047999c50700b9890a056d06f87a9df3ba66"|"rpc": "'$L1RPC'"|' /root/mee-node-deployment/chains-testnet/1.json
-
+echo ""
+echo ""
 cat /root/mee-node-deployment/chains-testnet/* | grep rpc
 
 docker compose down
