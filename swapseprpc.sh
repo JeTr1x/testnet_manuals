3b2014e5edab1c67e942174914d2ec49c410a2d4
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2025-05-14 12:31:50 +0300

    Update swapseprpc.sh

--- swapseprpc.sh
+++ swapseprpc.sh
@@ -3,9 +3,6 @@
 CHAINRPC="https://ethereum-sepolia.blockpi.network/v1/rpc/840aeeac9c2a1733b55ca7aca3d2d287797be72f"
 echo ""
 echo ""
-cat /root/mee-node-deployment/chains-testnet/* | grep rpc
-echo ""
-echo ""
 cd mee-node-deployment
 sed -i 's|"rpc":*.*"|"rpc": "'$CHAINRPC'"|' /root/mee-node-deployment/chains-testnet/11155111.json
 echo ""
