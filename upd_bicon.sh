a2628d85bca3249cd330b8eaa25e2ba1d5c50354
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2025-06-10 00:01:33 +0300

    Create upd_bicon.sh

---
+++ upd_bicon.sh
@@ -0,0 +1,4 @@
+cd mee-node-deployment
+sed -i 's|"rpc": *"https://ethereum-sepolia\.blockpi\.network/v1/rpc/[^"]*"|"rpc": "http://176.9.48.61:18545"|' chains-testnet/11155111.json
+sed -i 's|image: bcnmy/mee-node:1.1.19|image: bcnmy/mee-node:1.1.70|' docker-compose.yml
+docker-compose up -d
